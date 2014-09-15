dd49c695b274cbc99d32e344c49bca24d6d399d8
Chris Stark <starkfree@gmail.com>
2014-09-15 17:15:23 -0400

    Added SGD and POMBASE specific annotation supplement scripts

---
+++ SGD_parseGenes.py
@@ -0,0 +1,59 @@
+
+# Parse annotation from SGD and use it to
+# supplement the data already in place from
+# entrez gene.
+
+import Config
+import sys, string
+import MySQLdb
+import Database
+import gzip
+
+from classes import ModelOrganisms
+
+with Database.db as cursor :
+
+	sgd = ModelOrganisms.ModelOrganisms( Database.db, cursor )
+	sgdIDHash = sgd.buildSGDIDHash( )
+	
+	with open( Config.SGD_FEATURES, 'r' ) as file :
+		for line in file.readlines( ) :
+			
+			line = line.strip( )
+			
+			splitLine = line.split( "\t" )
+			sgdID = splitLine[0].strip( )
+			sgdType = splitLine[1].strip( )
+			
+			geneID = "none"
+			if sgdID not in sgdIDHash :
+				if "RETROTRANSPOSON" == sgdType.upper( ) :
+					cursor.execute( "INSERT INTO " + Config.DB_NAME + ".genes VALUES( '0', %s, 'ordered locus', %s, 'retrotransposon', '559292', 'active', NOW( ), NOW( ), 'SGD', '0' )", [orfName, sgdID] )
+					Database.db.commit( )
+					geneID = str(cursor.lastrowid)
+			else :
+			
+				# Process Addon Annotation
+				geneID = sgdIDHash[sgdID]
+				
+			if geneID != "none" :
+			
+				if "RETROTRANSPOSON" == sgdType.upper( ) :
+					cursor.execute( "UPDATE " + Config.DB_NAME + ".genes SET gene_updated = NOW( ) WHERE gene_id=%s", [geneID] )
+					Database.db.commit( )
+			
+				orfName = splitLine[3].strip( )
+				officialSymbol = splitLine[4].strip( )
+				aliases = (splitLine[5].strip( )).split( "|" )
+				additionalSGDIDs = (splitLine[7].strip( )).split( "|" )
+				definition = splitLine[15].strip( )
+					
+				sgd.processName( geneID, orfName, officialSymbol, "sgd-official", aliases )
+				sgd.processAddonSGDIDs( geneID, additionalSGDIDs )
+				sgd.processDefinition( geneID, definition, "SGD-DESCRIPTION" )
+				
+	cursor.execute( "INSERT INTO " + Config.DB_STATS + ".update_tracker VALUES ( '0', 'SGD_parseGenes', NOW( ) )" )
+	Database.db.commit( )
+	
+sys.exit( )
+			
