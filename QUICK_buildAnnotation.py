a0fbe7f6c908e716d9b62d333236345eb3497828
Chris Stark <starkfree@gmail.com>
2014-09-25 00:42:44 -0400

    Completed First Version of Quick ANnotation Script

--- QUICK_buildAnnotation.py
+++ QUICK_buildAnnotation.py
@@ -18,8 +18,9 @@ with Database.db as cursor :
 	cursor.execute( "TRUNCATE TABLE " + Config.DB_QUICK + ".quick_annotation" )
 	Database.db.commit( )
 	
-	cursor.execute( "SELECT * FROM " + Config.DB_NAME + ".genes WHERE gene_status='active' AND organism_id='7227' LIMIT 50" )
+	cursor.execute( "SELECT * FROM " + Config.DB_NAME + ".genes WHERE gene_status='active'" )
 	
+	insertCount = 0
 	for row in cursor.fetchall( ) :
 	
 		geneRecord = []
@@ -29,9 +30,13 @@ with Database.db as cursor :
 		
 		officialSymbol = str(row[1]).strip( )
 		refseqIDs = quick.fetchRefseqIDs( geneID )
+		uniprotIDs = quick.fetchUniprotIDs( refseqIDs )
 		
 		# Grab all aliases and a systematic name (if applicable)
 		systematicName, aliases = quick.fetchAliases( geneID, officialSymbol )
+		uniprotAliases, uniprotExternals, uniprotExternalTypes = quick.fetchUniprotNamesForGenes( uniprotIDs )
+		
+		aliases = set( aliases + uniprotAliases )
 		
 		if len( aliases ) <= 0 :
 			aliases = "-"
@@ -46,6 +51,8 @@ with Database.db as cursor :
 		
 		# Grab external Info
 		externalIDs, externalTypes = quick.fetchExternals( geneID, refseqIDs )
+		externalIDs = externalIDs + uniprotExternals
+		externalTypes = externalTypes + uniprotExternalTypes
 
 		if len( externalIDs ) <= 0 :
 			externalIDs = "-"
@@ -60,9 +67,45 @@ with Database.db as cursor :
 		geneRecord.append( str(row[4]) ) # Gene Type
 		geneRecord.append( str(row[9]) ) # Gene Source
 		
+		# GRAB Gene Ontology Details
+		goProcess, goComponent, goFunction = quick.fetchGO( geneID )
+		goCombined = {"IDS" : [], "NAMES" : [], "EVIDENCE" : []}
+		
+		goProcess, goCombined = quick.formatGOSet( goProcess, goCombined )
+		goComponent, goCombined = quick.formatGOSet( goComponent, goCombined )
+		goFunction, goCombined = quick.formatGOSet( goFunction, goCombined )
+		
+		if len(goCombined["IDS"]) > 0 :
+			goCombined["IDS"] = "|".join( goCombined["IDS"] )
+			goCombined["NAMES"] = "|".join( goCombined["NAMES"] )
+			goCombined["EVIDENCE"] = "|".join( goCombined["EVIDENCE"] )
+		else :
+			goCombined["IDS"] = "-"
+			goCombined["NAMES"] = "-"
+			goCombined["EVIDENCE"] = "-"
+			
+		geneRecord.extend( [goCombined["IDS"], goCombined["NAMES"], goCombined["EVIDENCE"]] )
+		geneRecord.extend( [goProcess["IDS"], goProcess["NAMES"], goProcess["EVIDENCE"]] )
+		geneRecord.extend( [goComponent["IDS"], goComponent["NAMES"], goComponent["EVIDENCE"]] )
+		geneRecord.extend( [goFunction["IDS"], goFunction["NAMES"], goFunction["EVIDENCE"]] )
+		
 		# Get organism info out of the hash
 		(orgID, orgEntrezID, orgUniprotID, orgName, orgOfficial, orgAbbr, orgStrain) = orgHash[str(row[5])]
 		geneRecord.extend( [str(orgID), orgName, orgOfficial, orgAbbr, orgStrain] )
 		
-		print geneRecord
-		print "----------------------------"
+		# Interaction Count
+		geneRecord.append( "0" )
+		
+		sqlFormat = ",".join( ['%s'] * len(geneRecord) )
+		cursor.execute( "INSERT INTO " + Config.DB_QUICK + ".quick_annotation VALUES (%s)" % sqlFormat, tuple(geneRecord) )
+		insertCount = insertCount + 1
+		
+		if 0 == (insertCount % Config.DB_COMMIT_COUNT ) :
+			Database.db.commit( )
+			
+	Database.db.commit( )
+	
+	cursor.execute( "INSERT INTO " + Config.DB_STATS + ".update_tracker VALUES ( '0', 'QUICK_buildAnnotation', NOW( ) )" )
+	Database.db.commit( )
+	
+sys.exit( )
--- classes/Quick.py
+++ classes/Quick.py
@@ -10,6 +10,8 @@ class Quick( ) :
 	def __init__( self, db, cursor ) :
 		self.db = db
 		self.cursor = cursor
+		self.goDefs = self.fetchGODefinitionHash( )
+		self.goEvidence = self.fetchGOEvidenceHash( )
 		
 	def fetchOrganismHash( self ) :
 		
@@ -20,6 +22,79 @@ class Quick( ) :
 			organismHash[str(row[0])] = row
 			
 		return organismHash
+		
+	def fetchGODefinitionHash( self ) :
+	
+		self.cursor.execute( "SELECT go_id, go_name, go_type FROM " + Config.DB_NAME + ".go_definitions" )
+		
+		goHash = { }
+		for row in self.cursor.fetchall( ) :
+			goHash[str(row[0])] = row
+			
+		return goHash
+		
+	def fetchGOEvidenceHash( self ) :
+	
+		self.cursor.execute( "SELECT go_evidence_code_id, go_evidence_code_symbol FROM " + Config.DB_NAME + ".go_evidence_codes" )
+		
+		goHash = { }
+		for row in self.cursor.fetchall( ) :
+			goHash[str(row[0])] = row[1]
+			
+		return goHash
+		
+	def formatGOSet( self, goSet, goCombined ) :
+	
+		if len(goSet["IDS"]) > 0 :
+			goCombined["IDS"] = goCombined["IDS"] + goSet["IDS"]
+			goCombined["NAMES"] = goCombined["NAMES"] + goSet["NAMES"]
+			goCombined["EVIDENCE"] = goCombined["EVIDENCE"] + goSet["EVIDENCE"]
+			
+			goSet["IDS"] = "|".join( goSet["IDS"] )
+			goSet["NAMES"] = "|".join( goSet["NAMES"] )
+			goSet["EVIDENCE"] = "|".join( goSet["EVIDENCE"] )
+			
+		else :
+			
+			goSet["IDS"] = "-"
+			goSet["NAMES"] = "-"
+			goSet["EVIDENCE"] = "-"
+			
+		return goSet, goCombined
+		
+	def fetchGO( self, geneID ) :
+	
+		goProcess = { "IDS" : [], "NAMES" : [], "EVIDENCE" : [] }
+		goComponent = { "IDS" : [], "NAMES" : [], "EVIDENCE" : [] }
+		goFunction = { "IDS" : [], "NAMES" : [], "EVIDENCE" : [] }
+		
+		if len(geneID) > 0 :
+			self.cursor.execute( "SELECT go_id, go_evidence_code_id FROM " + Config.DB_NAME + ".gene_go WHERE gene_id=%s AND gene_go_status='active'", [geneID] )
+		
+			for row in self.cursor.fetchall( ) :
+				(goID, goName, goType) = self.goDefs[str(row[0])]
+				goEvidence = self.goEvidence[str(row[1])]
+				
+				goCompare = goName.upper( )
+				if "BIOLOGICAL_PROCESS" != goCompare and "CELLULAR_COMPONENT" != goCompare and "MOLECULAR_FUNCTION" != goCompare :
+					
+					goCompare = goType.upper( )
+					if "BIOLOGICAL_PROCESS" == goCompare :
+						goProcess["IDS"].append( str(goID) )
+						goProcess["EVIDENCE"].append( goEvidence )
+						goProcess["NAMES"].append( goName )
+					
+					elif "CELLULAR_COMPONENT" == goCompare :
+						goComponent["IDS"].append( str(goID) )
+						goComponent["EVIDENCE"].append( goEvidence )
+						goComponent["NAMES"].append( goName )
+						
+					elif "MOLECULAR_FUNCTION" == goCompare :
+						goFunction["IDS"].append( str(goID) )
+						goFunction["EVIDENCE"].append( goEvidence )
+						goFunction["NAMES"].append( goName )	
+		
+		return goProcess, goComponent, goFunction
 		
 	def fetchExternals( self, geneID, refseqIDs ) :
 	
@@ -35,11 +110,15 @@ class Quick( ) :
 				
 			if len(refseqIDs) > 0 :
 				sqlFormat = ",".join( ['%s'] * len(refseqIDs) )
-				self.cursor.execute( "SELECT refseq_accession, refseq_gi FROM " + Config.DB_NAME + ".refseq WHERE refseq_id IN (%s) and refseq_status='active'" % sqlFormat, tuple(refseqIDs) )
+				self.cursor.execute( "SELECT refseq_accession, refseq_gi, refseq_version FROM " + Config.DB_NAME + ".refseq WHERE refseq_id IN (%s) and refseq_status='active'" % sqlFormat, tuple(refseqIDs) )
 	
 				for row in self.cursor.fetchall( ) :
 					externalIDSet.extend( [str(row[0]), str(row[1])] )
 					externalTypeSet.extend( ["REFSEQ-PROTEIN-ACCESSION", "REFSEQ-PROTEIN-GI"] )
+					
+					for version in range( 0, row[2] ) :
+						externalIDSet.append( str(row[0]) + "." + str(version+1) )
+						externalTypeSet.append( "REFSEQ-PROTEIN-ACCESSION-VERSIONED" )
 					
 				self.cursor.execute( "SELECT refseq_identifier_value, refseq_identifier_type FROM " + Config.DB_NAME + ".refseq_identifiers WHERE refseq_id IN (%s) and refseq_identifier_status='active'" % sqlFormat, tuple(refseqIDs) )
 	
@@ -58,7 +137,7 @@ class Quick( ) :
 		
 	def fetchAliases( self, geneID, officialSymbol ) :
 	
-		aliases = set( )
+		aliases = []
 		systematicName = "-"
 		
 		if len(geneID) > 0 :
@@ -70,19 +149,71 @@ class Quick( ) :
 					systematicName = str(row[0])
 				else :
 					if row[0].upper( ) != officialSymbol.upper( ) :
-						aliases.add( str(row[0]) )
+						aliases.append( str(row[0]) )
 						
 		return systematicName, aliases
 		
 	def fetchRefseqIDs( self, geneID ) :
 		
 		refseqIDs = set( )
-		self.cursor.execute( "SELECT refseq_id FROM " + Config.DB_NAME + ".gene_refseqs WHERE gene_id=%s and gene_refseq_status='active'", [geneID] )
 		
-		for row in self.cursor.fetchall( ) :
-			refseqIDs.add( str(row[0]) )
+		if len(geneID) > 0 :
+			self.cursor.execute( "SELECT refseq_id FROM " + Config.DB_NAME + ".gene_refseqs WHERE gene_id=%s and gene_refseq_status='active'", [geneID] )
+		
+			for row in self.cursor.fetchall( ) :
+				refseqIDs.add( str(row[0]) )
 				
 		return refseqIDs
+		
+	def fetchUniprotIDs( self, refseqIDs ) :
+	
+		uniprotIDs = set( )
+		
+		if len(refseqIDs) > 0 :
+			sqlFormat = ",".join( ['%s'] * len(refseqIDs) )
+			self.cursor.execute( "SELECT uniprot_id FROM " + Config.DB_NAME + ".protein_mapping WHERE refseq_id IN (%s) and protein_mapping_status='active'" % sqlFormat, tuple(refseqIDs) )
+		
+			for row in self.cursor.fetchall( ) :
+				uniprotIDs.add( str(row[0]) )
+				
+		return uniprotIDs
+		
+	def fetchUniprotNamesForGenes( self, uniprotIDs ) :
+
+		uniprotAliases = []
+		uniprotExternals = []
+		uniprotExternalTypes = []
+		
+		if len(uniprotIDs) > 0 :
+			sqlFormat = ",".join( ['%s'] * len(uniprotIDs) )
+			self.cursor.execute( "SELECT uniprot_identifier_value, uniprot_name, uniprot_source FROM " + Config.DB_NAME + ".uniprot WHERE uniprot_id IN (%s) AND uniprot_status='active'" % sqlFormat, tuple(uniprotIDs) )
+
+			for row in self.cursor.fetchall( ) :
+				uniprotAliases.append( row[1] )
+				uniprotExternals.append( row[0] )
+				uniprotExternalTypes.append( row[2].upper( ) )
+				
+			self.cursor.execute( "SELECT uniprot_alias_value, uniprot_alias_type FROM " + Config.DB_NAME + ".uniprot_aliases WHERE uniprot_id IN (%s) AND uniprot_alias_type != 'primary-accession' AND uniprot_alias_status='active'" % sqlFormat, tuple(uniprotIDs) )
+			
+			for row in self.cursor.fetchall( ) :
+				if 'ACCESSION' == row[1].upper( ) :
+					uniprotExternals.append( row[0] )
+					uniprotExternalTypes.append( 'UNIPROT-ACCESSION' )
+				else :
+					uniprotAliases.append( row[0] )
+					
+			self.cursor.execute( "SELECT uniprot_isoform_accession, uniprot_isoform_number FROM " + Config.DB_NAME + ".uniprot_isoforms WHERE uniprot_id IN (%s) AND uniprot_isoform_status = 'active'" % sqlFormat, tuple(uniprotIDs) )
+					
+			for row in self.cursor.fetchall( ) :
+				
+				isoform = row[0] + "-" + str(row[1])
+				
+				if isoform not in uniprotExternals :
+				
+					uniprotExternals.append( isoform )
+					uniprotExternalTypes.append( 'UNIPROT-ISOFORM' )
+					
+		return uniprotAliases, uniprotExternals, uniprotExternalTypes
 		
 	def fetchDescription( self, geneID, refseqIDs ) :
 		
