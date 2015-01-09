5bdb959e2095911a249820c61da26e3dc74750ac
Chris Stark <starkfree@gmail.com>
2015-01-09 01:07:45 -0500

    Fixed PTM Swap Script

--- TOOLS_swapPTMs.py
+++ TOOLS_swapPTMs.py
@@ -16,5 +16,35 @@ inputArgs = vars( argParser.parse_args( ) )
 
 with Database.db as cursor :
 
+	disabledPTMs = set( )
+	swappedPTMs = set( )
+	
+	with open( inputArgs['inputFile'], 'r' ) as file :
+		for line in file.readlines( ) :
+			
+			line = line.strip( )
+				
+			splitLine = line.split( "\t" )
+			ptmID = splitLine[0].strip( )
+			oldSeqID = splitLine[1].strip( )
+			oldLoc = splitLine[2].strip( )
+			oldRes = splitLine[3].strip( )
+			newSeqID = splitLine[4].strip( )
+			newLoc = splitLine[5].strip( )
+			newRes = splitLine[6].strip( )
+			
+			if "DEAD" == newSeqID.upper( ) :
+				cursor.execute( "UPDATE " + Config.DB_IMS + ".ptms SET ptm_status='inactive' WHERE ptm_id=%s", [ptmID] )
+				disabledPTMs.add( ptmID )
+			else :
+				cursor.execute( "UPDATE " + Config.DB_IMS + ".ptms SET refseq_protein_id=%s, ptm_residue_location=%s, ptm_residue=%s WHERE ptm_id=%s", [newSeqID, newLoc, newRes, ptmID] )
+				swappedPTMs.add( ptmID )
+			
+			Database.db.commit( )
+	
+	print "DISABLED : " + str(len(disabledPTMs)) + " PTMs"
+	print "SWAPPED  : " + str(len(swappedPTMs)) +  " PTMs"
+	
+	Database.db.commit( )
 				
 sys.exit( )
